3fa70f5ca92a76b06b1f793b37da2481209aaa11
Travis Cardwell <travis.cardwell@extellisys.com>
2014-11-05 20:46:15 +0900

    add project stub

---
+++ journal.hs
@@ -0,0 +1,17 @@
+------------------------------------------------------------------------------
+-- |
+--  Module      : Main
+--  Description : an example Hakyll Journal site
+--  Copyright   : (c) 2014, Travis Cardwell and Rob Nugen
+--  License     : MIT
+--  Maintainer  : Travis Cardwell <travis.cardwell@extellisys.com>
+--  Stability   : experimental
+--
+-- This is an example site using Hakyll Journal, a Hakyll module for adding a
+-- journal/diary to your static website.
+------------------------------------------------------------------------------
+
+module Main where
+
+main :: IO ()
+main = putStrLn "hakyll-journal-example"
